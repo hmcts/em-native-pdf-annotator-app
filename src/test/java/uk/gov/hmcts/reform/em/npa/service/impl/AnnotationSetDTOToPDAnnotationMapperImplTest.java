0b5119be30de8a09d8e1d6570cd44dc349d50d0b
Pawel Kaczmarek <pawel.kaczmarek@gmail.com>
2018-10-03 10:27:55 +0100

    tests, fixes

---
+++ src/test/java/uk/gov/hmcts/reform/em/npa/service/impl/AnnotationSetDTOToPDAnnotationMapperImplTest.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.em.npa.service.impl;
+
+import io.jsonwebtoken.lang.Assert;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.junit.Test;
+import uk.gov.hmcts.reform.em.npa.service.dto.external.annotation.AnnotationDTO;
+import uk.gov.hmcts.reform.em.npa.service.dto.external.annotation.AnnotationSetDTO;
+import uk.gov.hmcts.reform.em.npa.service.dto.external.annotation.RectangleDTO;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class AnnotationSetDTOToPDAnnotationMapperImplTest {
+
+    AnnotationSetDTOToPDAnnotationMapperImpl annotationSetDTOToPDAnnotationMapperImpl =
+        new AnnotationSetDTOToPDAnnotationMapperImpl();
+
+    @Test
+    public void testSuccessfulRun() throws Exception {
+        Set<AnnotationDTO> annotationSet = new HashSet<>();
+
+        AnnotationDTO annotationDTO = new AnnotationDTO();
+        annotationDTO.setPage(0);
+        annotationDTO.setAnnotationType("height");
+
+        Set<RectangleDTO> rectangles = new HashSet<>();
+
+        RectangleDTO rectangleDTO = new RectangleDTO();
+        rectangleDTO.setX(0f);
+        rectangleDTO.setY(0f);
+        rectangleDTO.setHeight(10f);
+        rectangleDTO.setWidth(10f);
+        rectangles.add(rectangleDTO);
+
+        annotationDTO.setRectangles(rectangles);
+
+        annotationSet.add(annotationDTO);
+        Map<Integer, List<PDAnnotation>> result = annotationSetDTOToPDAnnotationMapperImpl.toNativeAnnotationsPerPage(annotationSet);
+
+        Assert.notNull(result.get(0).get(0));
+    }
+
+}
