b40a3a77b87dd2f049253cfb18b8dbbc23071cd2
pawelkaczmarekuk <pawel.kaczmarek@gmail.com>
2018-10-16 17:41:04 +0100

    coverage

---
+++ src/test/java/uk/gov/hmcts/reform/em/npa/service/impl/PdfAnnotatorImplTest.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.em.npa.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.reform.em.npa.Application;
+import uk.gov.hmcts.reform.em.npa.service.AnnotationSetDTOToPDAnnotationMapper;
+import uk.gov.hmcts.reform.em.npa.service.PdfAnnotator;
+
+import java.io.File;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@Transactional
+public class PdfAnnotatorImplTest {
+
+    @Autowired
+    private PdfAnnotator pdfAnnotator;
+
+    @Test(expected = DocumentTaskProcessingException.class)
+    public void annotatePdf() throws Exception {
+        pdfAnnotator.annotatePdf(new File("xyz.abc"), null);
+    }
+}
