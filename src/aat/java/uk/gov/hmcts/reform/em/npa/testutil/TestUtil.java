84adbbad0ef0b8288a9626568fed01895e7e1954
pawelkaczmarekuk <pawel.kaczmarek@gmail.com>
2019-01-22 11:55:55 +0000

    idam fix

--- src/aat/java/uk/gov/hmcts/reform/em/npa/testutil/TestUtil.java
+++ src/aat/java/uk/gov/hmcts/reform/em/npa/testutil/TestUtil.java
@@ -159,7 +159,7 @@ public class TestUtil {
 
     private Integer findUserIdByUserEmail(String email) {
         return RestAssured
-                .get(Env.getIdamURL() + "/users?email=" + email)
+                .get(Env.getIdamURL() + "/testing-support/accounts/" + email)
                 .getBody()
                 .jsonPath()
                 .get("id");
