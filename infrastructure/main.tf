f19c118988cea3b2666cf1b779a488d2151274b2
Linus Norton <linusnorton@gmail.com>
2019-03-27 10:18:22 +0000

    update ase name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,6 @@
 locals {
   app_full_name = "${var.product}-${var.component}"
-  ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  ase_name = "core-compute-${var.env}"
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
   shared_vault_name = "${var.shared_product_name}-${local.local_env}"
   tags = "${merge(var.common_tags, map("Team Contact", "#rpe"))}"
