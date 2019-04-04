a8b3cbc9e034ef270c4837a86e7ba96569639d4b
Linus Norton <linusnorton@gmail.com>
2019-04-04 15:47:41 +0100

    add idam api url

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -8,6 +8,10 @@ output "vaultName" {
   value = "${module.local_key_vault.key_vault_name}"
 }
 
+output "idam_api_base_uri" {
+ value = "${var.idam_api_base_uri}"
+}
+
 output "enable_idam_healthcheck" {
   value = "${var.enable_idam_healthcheck}"
 }
