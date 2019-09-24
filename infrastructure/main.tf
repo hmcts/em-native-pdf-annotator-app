30700d809d0d15d0cec10332153b448cac2246a0
Linus Norton <linusnorton@gmail.com>
2019-09-24 08:51:08 +0100

    switch to cnp

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -118,7 +118,7 @@ data "azurerm_key_vault" "shared_key_vault" {
 }
 
 module "local_key_vault" {
-  source = "git@github.com:hmcts/moj-module-key-vault?ref=master"
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   product = "${local.app_full_name}"
   env = "${var.env}"
   tenant_id = "${var.tenant_id}"
