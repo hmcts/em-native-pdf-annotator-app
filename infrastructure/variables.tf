22eb8d3b355a1ac134d6223833e15f800b147f56
Megan <meganaliceslattery@gmail.com>
2020-03-10 11:51:34 +0000

    EM-1877: Update infrastructure files etc

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -60,6 +60,14 @@ variable "idam_api_base_uri" {
   default = "http://betaDevBccidamAppLB.reform.hmcts.net:80"
 }
 
+variable "open_id_api_base_uri" {
+  default = "idam-api"
+}
+
+variable "oidc_issuer_base_uri" {
+  default = "idam-api"
+}
+
 variable "s2s_url" {
   default = "rpe-service-auth-provider"
 }
