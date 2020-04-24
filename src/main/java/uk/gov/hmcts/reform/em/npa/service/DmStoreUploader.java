8b5abf2376ef18513e3d33867162833be2412111
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-04-24 08:16:44 +0100

    Added in logic to upload the redacted file

--- src/main/java/uk/gov/hmcts/reform/em/npa/service/DmStoreUploader.java
+++ src/main/java/uk/gov/hmcts/reform/em/npa/service/DmStoreUploader.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.em.npa.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import uk.gov.hmcts.reform.em.npa.domain.DocumentTask;
 import uk.gov.hmcts.reform.em.npa.service.impl.DocumentTaskProcessingException;
 
@@ -7,6 +8,8 @@ import java.io.File;
 
 public interface DmStoreUploader {
 
-    void uploadFile(File file, DocumentTask documentTask) throws DocumentTaskProcessingException;;
+    void uploadFile(File file, DocumentTask documentTask) throws DocumentTaskProcessingException;
+
+    JsonNode uploadNewDocument(File file) throws DocumentTaskProcessingException;
 
 }
--- src/main/java/uk/gov/hmcts/reform/em/npa/service/impl/DmStoreUploaderImpl.java
+++ src/main/java/uk/gov/hmcts/reform/em/npa/service/impl/DmStoreUploaderImpl.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.em.npa.service.impl;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.*;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
@@ -28,13 +30,16 @@ public class DmStoreUploaderImpl implements DmStoreUploader {
 
     private final SecurityUtils securityUtils;
 
+    private final ObjectMapper objectMapper;
+
     public DmStoreUploaderImpl(OkHttpClient okHttpClient, AuthTokenGenerator authTokenGenerator,
                                @Value("${dm-store-app.base-url}") String dmStoreAppBaseUrl,
-                               SecurityUtils securityUtils) {
+                               SecurityUtils securityUtils, ObjectMapper objectMapper) {
         this.okHttpClient = okHttpClient;
         this.authTokenGenerator = authTokenGenerator;
         this.dmStoreAppBaseUrl = dmStoreAppBaseUrl;
         this.securityUtils = securityUtils;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -44,6 +49,43 @@ public class DmStoreUploaderImpl implements DmStoreUploader {
         } else {
             uploadNewDocument(file, documentTask);
         }
+    }
+
+    @Override
+    public JsonNode uploadNewDocument(File file) throws DocumentTaskProcessingException {
+
+        // DRY ?
+        try {
+
+            MultipartBody requestBody = new MultipartBody
+                .Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("classification", "PUBLIC")
+                .addFormDataPart("files", file.getName(), RequestBody.create(MediaType.get("application/pdf"), file))
+                .build();
+
+            Request request = new Request.Builder()
+                .addHeader("user-id", securityUtils.getCurrentUserLogin().orElse(Constants.ANONYMOUS_USER))
+                .addHeader("user-roles", "caseworker")
+                .addHeader("ServiceAuthorization", authTokenGenerator.generate())
+                .url(dmStoreAppBaseUrl + dmStoreUploadEndpoint)
+                .method("POST", requestBody)
+                .build();
+
+            Response response = okHttpClient.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+
+                return objectMapper.readTree(response.body().string());
+
+            } else {
+                throw new DocumentTaskProcessingException("Couldn't upload the file. Response code: " + response.code(), null);
+            }
+
+        } catch (RuntimeException | IOException e) {
+            throw new DocumentTaskProcessingException(String.format("Couldn't upload the file:  %s", e.getMessage()), e);
+        }
+
     }
 
     private void uploadNewDocument(File file, DocumentTask documentTask) throws DocumentTaskProcessingException {
