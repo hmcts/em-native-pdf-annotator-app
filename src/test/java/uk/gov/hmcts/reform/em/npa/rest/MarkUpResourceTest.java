06ae15ec0bebf6231d2063df0577bce053901118
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-04-27 17:32:30 +0100

    Added in Test case

---
+++ src/test/java/uk/gov/hmcts/reform/em/npa/rest/MarkUpResourceTest.java
@@ -0,0 +1,131 @@
+package uk.gov.hmcts.reform.em.npa.rest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import uk.gov.hmcts.reform.em.npa.domain.MarkUpDTO;
+import uk.gov.hmcts.reform.em.npa.rest.errors.EmptyResponseException;
+import uk.gov.hmcts.reform.em.npa.service.MarkUpService;
+
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+public class MarkUpResourceTest {
+
+    @InjectMocks
+    private MarkUpResource markUpResource;
+
+    @Mock
+    private MarkUpService markUpService;
+
+    @Mock
+    private BindingResult result;
+
+    @Mock
+    private Pageable pageable;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void createMarkUpSuccess() throws URISyntaxException {
+
+        MarkUpDTO markUpDTO = createMarkUpDTO();
+
+        Mockito.when(markUpService.save(markUpDTO)).thenReturn(markUpDTO);
+
+        ResponseEntity<MarkUpDTO>  responseEntity = markUpResource.createMarkUp(markUpDTO, result);
+
+        MarkUpDTO response = responseEntity.getBody();
+        Assert.assertEquals(markUpDTO.getDocumentId(), response.getDocumentId());
+        Assert.assertEquals(markUpDTO.getId(), response.getId());
+        Assert.assertEquals(markUpDTO.getHeight(), response.getHeight());
+        Assert.assertEquals(markUpDTO.getWidth(), response.getWidth());
+        Assert.assertEquals(markUpDTO.getXCoordinate(), response.getXCoordinate());
+        Assert.assertEquals(markUpDTO.getYCoordinate(), response.getYCoordinate());
+
+        Mockito.verify(markUpService, Mockito.atLeast(1)).save(markUpDTO);
+    }
+
+    @Test
+    public void updateMarkUpSuccess() throws URISyntaxException {
+
+        MarkUpDTO markUpDTO = createMarkUpDTO();
+
+        Mockito.when(markUpService.save(markUpDTO)).thenReturn(markUpDTO);
+
+        ResponseEntity<MarkUpDTO>  responseEntity = markUpResource.updateMarkUp(markUpDTO, result);
+
+        MarkUpDTO response = responseEntity.getBody();
+        Assert.assertEquals(markUpDTO.getDocumentId(), response.getDocumentId());
+        Assert.assertEquals(markUpDTO.getId(), response.getId());
+        Assert.assertEquals(markUpDTO.getHeight(), response.getHeight());
+        Assert.assertEquals(markUpDTO.getWidth(), response.getWidth());
+        Assert.assertEquals(markUpDTO.getXCoordinate(), response.getXCoordinate());
+        Assert.assertEquals(markUpDTO.getYCoordinate(), response.getYCoordinate());
+
+        Mockito.verify(markUpService, Mockito.atLeast(1)).save(markUpDTO);
+    }
+
+    @Test
+    public void testGetAllDocumentMarkUpsSuccess() {
+
+        UUID id =  UUID.randomUUID();
+        List<MarkUpDTO> markUps = Arrays.asList(createMarkUpDTO());
+        Page<MarkUpDTO> markUpDtos = new PageImpl<>(markUps);
+
+        Mockito.when(markUpService.findAllByDocumentId(id, pageable)).thenReturn(markUpDtos);
+        ResponseEntity<List<MarkUpDTO>> response = markUpResource.getAllDocumentMarkUps(id, pageable);
+
+        Mockito.verify(markUpService, Mockito.atLeast(1)).findAllByDocumentId(id, pageable);
+    }
+
+    @Test(expected = EmptyResponseException.class)
+    public void testGetAllDocumentMarkUpsFailure() {
+
+        UUID id =  UUID.randomUUID();
+
+        Page<MarkUpDTO> markUpDtos = new PageImpl<>(Collections.emptyList());
+
+        Mockito.when(markUpService.findAllByDocumentId(id, pageable)).thenReturn(markUpDtos);
+        markUpResource.getAllDocumentMarkUps(id, pageable);
+    }
+
+    @Test
+    public void testDeleteMarkUpSuccess() {
+
+        UUID id =  UUID.randomUUID();
+        Mockito.doNothing().when(markUpService).delete(id);
+
+        ResponseEntity<Void> response = markUpResource.deleteMarkUp(id);
+
+        Mockito.verify(markUpService, Mockito.atLeast(1)).delete(id);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    private MarkUpDTO createMarkUpDTO() {
+        MarkUpDTO markUpDTO = new MarkUpDTO();
+        markUpDTO.setDocumentId(UUID.randomUUID());
+        markUpDTO.setId(UUID.randomUUID());
+        markUpDTO.setHeight(10);
+        markUpDTO.setWidth(10);
+        markUpDTO.setXCoordinate(20);
+        markUpDTO.setYCoordinate(30);
+        return markUpDTO;
+    }
+}
--- src/test/java/uk/gov/hmcts/reform/em/npa/service/impl/DmStoreUploaderImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/npa/service/impl/DmStoreUploaderImplTest.java
@@ -1,30 +1,212 @@
 package uk.gov.hmcts.reform.em.npa.service.impl;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.*;
+import org.apache.pdfbox.io.IOUtils;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.em.npa.Application;
 import uk.gov.hmcts.reform.em.npa.TestSecurityConfiguration;
+import uk.gov.hmcts.reform.em.npa.config.security.SecurityUtils;
 import uk.gov.hmcts.reform.em.npa.domain.DocumentTask;
 import uk.gov.hmcts.reform.em.npa.service.DmStoreUploader;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {Application.class, TestSecurityConfiguration.class})
-@Transactional
 public class DmStoreUploaderImplTest {
 
-    @Autowired
+    private static final String PDF_FILENAME = "annotationTemplate.pdf";
+
     DmStoreUploader dmStoreUploader;
 
-    @Test(expected = DocumentTaskProcessingException.class)
-    public void uploadFile() throws Exception {
+    @Autowired
+    private SecurityUtils securityUtils;
+
+    private static Response interceptSuccess (Interceptor.Chain chain) throws IOException {
+
+        if (chain.request().url().toString().endsWith("/binary")) {
+            InputStream file = ClassLoader.getSystemResourceAsStream(PDF_FILENAME);
+
+            return new Response.Builder()
+                .body(ResponseBody.create(MediaType.get("application/pdf"), IOUtils.toByteArray(file)))
+                .request(chain.request())
+                .message("")
+                .code(200)
+                .protocol(Protocol.HTTP_2)
+                .build();
+        } else {
+            return new Response.Builder()
+                .body(ResponseBody.create(MediaType.get("application/json"),
+                    "{\n" +
+                        "  \"_embedded\": {\n" +
+                        "    \"documents\": [\n" +
+                        "      {\n" +
+                        "        \"modifiedOn\": \"2020-04-23T14:37:02+0000\",\n" +
+                        "        \"size\": 19496,\n" +
+                        "        \"createdBy\": \"7f0fd7bf-48c0-4462-9056-38c1190e391f\",\n" +
+                        "        \"_links\": {\n" +
+                        "          \"thumbnail\": {\n" +
+                        "            \"href\": \"http://localhost:4603/documents/0e38e3ad-171f-4d27-bf54-e41f2ed744eb/thumbnail\"\n" +
+                        "          },\n" +
+                        "          \"binary\": {\n" +
+                        "            \"href\": \"http://localhost:4603/documents/0e38e3ad-171f-4d27-bf54-e41f2ed744eb/binary\"\n" +
+                        "          },\n" +
+                        "          \"self\": {\n" +
+                        "            \"href\": \"http://localhost:4603/documents/0e38e3ad-171f-4d27-bf54-e41f2ed744eb\"\n" +
+                        "          }\n" +
+                        "        },\n" +
+                        "        \"_embedded\": {\n" +
+                        "          \"allDocumentVersions\": {\n" +
+                        "            \"_embedded\": {\n" +
+                        "              \"documentVersions\": [\n" +
+                        "                {\n" +
+                        "                  \"size\": 19496,\n" +
+                        "                  \"createdBy\": \"7f0fd7bf-48c0-4462-9056-38c1190e391f\",\n" +
+                        "                  \"_links\": {\n" +
+                        "                    \"thumbnail\": {\n" +
+                        "                      \"href\": \"http://localhost:4603/documents/0e38e3ad-171f-4d27-bf54-e41f2ed744eb/versions/da13dae7-f2bc-4a43-937c-83a255f2f72f/thumbnail\"\n" +
+                        "                    },\n" +
+                        "                    \"document\": {\n" +
+                        "                      \"href\": \"http://localhost:4603/documents/0e38e3ad-171f-4d27-bf54-e41f2ed744eb\"\n" +
+                        "                    },\n" +
+                        "                    \"binary\": {\n" +
+                        "                      \"href\": \"http://localhost:4603/documents/0e38e3ad-171f-4d27-bf54-e41f2ed744eb/versions/da13dae7-f2bc-4a43-937c-83a255f2f72f/binary\"\n" +
+                        "                    },\n" +
+                        "                    \"self\": {\n" +
+                        "                      \"href\": \"http://localhost:4603/documents/0e38e3ad-171f-4d27-bf54-e41f2ed744eb/versions/da13dae7-f2bc-4a43-937c-83a255f2f72f\"\n" +
+                        "                    }\n" +
+                        "                  },\n" +
+                        "                  \"originalDocumentName\": \"stitched9163237694642183694.pdf\",\n" +
+                        "                  \"mimeType\": \"application/pdf\",\n" +
+                        "                  \"createdOn\": \"2020-04-23T14:37:02+0000\"\n" +
+                        "                }\n" +
+                        "              ]\n" +
+                        "            }\n" +
+                        "          }\n" +
+                        "        },\n" +
+                        "        \"lastModifiedBy\": \"7f0fd7bf-48c0-4462-9056-38c1190e391f\",\n" +
+                        "        \"originalDocumentName\": \"stitched9163237694642183694.pdf\",\n" +
+                        "        \"mimeType\": \"application/pdf\",\n" +
+                        "        \"classification\": \"PUBLIC\",\n" +
+                        "        \"createdOn\": \"2020-04-23T14:37:02+0000\"\n" +
+                        "      }\n" +
+                        "    ]\n" +
+                        "  }\n" +
+                        "}"))
+                .request(chain.request())
+                .message("")
+                .code(200)
+                .protocol(Protocol.HTTP_2)
+                .build();
+        }
+
+    }
+
+    private static Response interceptFailure (Interceptor.Chain chain) throws IOException {
+
+        InputStream file = ClassLoader.getSystemResourceAsStream(PDF_FILENAME);
+
+        return new Response.Builder()
+            .body(ResponseBody.create(MediaType.get("application/pdf"), IOUtils.toByteArray(file)))
+            .request(chain.request())
+            .message("")
+            .code(404)
+            .protocol(Protocol.HTTP_2)
+            .build();
+    }
+
+
+    private void setUpSuccess() {
+        OkHttpClient http = new OkHttpClient
+            .Builder()
+            .addInterceptor(DmStoreUploaderImplTest::interceptSuccess)
+            .build();
+
+        dmStoreUploader = new DmStoreUploaderImpl(http, () -> "auth", "https://someurl.com",
+            securityUtils, new ObjectMapper());
+    }
+
+    private void setUpFailure() {
+        OkHttpClient http = new OkHttpClient
+            .Builder()
+            .addInterceptor(DmStoreUploaderImplTest::interceptFailure)
+            .build();
+
+        dmStoreUploader = new DmStoreUploaderImpl(http, () -> "auth", "https://someurl.com",
+            securityUtils, new ObjectMapper());
+    }
+
+    @Test
+    public void testUploadFileSuccess() throws Exception {
+
+        setUpSuccess();
+
         DocumentTask task = new DocumentTask();
         task.setJwt("xxx");
         dmStoreUploader.uploadFile(new File("xyz.abc"), task);
+        Assert.assertEquals("0e38e3ad-171f-4d27-bf54-e41f2ed744eb", task.getOutputDocumentId());
+    }
+
+    @Test(expected = DocumentTaskProcessingException.class)
+    public void testUploadFileFailure() throws Exception {
+
+        setUpFailure();
+
+        DocumentTask task = new DocumentTask();
+        task.setJwt("xxx");
+        dmStoreUploader.uploadFile(new File("xyz.abc"), task);
+    }
+
+    @Test
+    public void testUploadNewDocumentVersionSuccess() throws Exception {
+
+        setUpSuccess();
+
+        DocumentTask task = new DocumentTask();
+        task.setJwt("xxx");
+        task.setOutputDocumentId("http://localhost/v1");
+        dmStoreUploader.uploadFile(new File("xyz.abc"), task);
+        Assert.assertEquals("http://localhost/v1", task.getOutputDocumentId());
+    }
+
+    @Test(expected = DocumentTaskProcessingException.class)
+    public void testUploadNewDocumentVersionFailure() throws Exception {
+
+        setUpFailure();
+
+        DocumentTask task = new DocumentTask();
+        task.setJwt("xxx");
+        task.setOutputDocumentId("http://localhost/v1");
+        dmStoreUploader.uploadFile(new File("xyz.abc"), task);
+    }
+
+    @Test
+    public void  testUploadDocumentSuccess() throws Exception {
+
+        setUpSuccess();
+
+        JsonNode response = dmStoreUploader.uploadDocument(new File("xyz.abc"));
+
+        Assert.assertNotNull(response);
+        String docUrl = response.at("/_embedded/documents").get(0).at("/_links/self/href").asText();
+        Assert.assertNotNull(docUrl);
+        Assert.assertEquals("http://localhost:4603/documents/0e38e3ad-171f-4d27-bf54-e41f2ed744eb", docUrl);
+    }
+
+    @Test(expected = DocumentTaskProcessingException.class)
+    public void  testUploadDocumentFailure() throws Exception {
+
+        setUpFailure();
+
+        dmStoreUploader.uploadDocument(new File("xyz.abc"));
     }
 }
