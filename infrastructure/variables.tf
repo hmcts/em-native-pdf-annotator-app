87fab8be23a647c29e83dcb7ef19ce660e40511a
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-10-05 07:42:38 +0100

    Updated azurerm details

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,14 +1,10 @@
-variable "product" {
-  type = "string"
-}
+variable "product" {}
 
 variable "shared_product_name" {
   default = "rpa"
 }
 
-variable "component" {
-  type = "string"
-}
+variable "component" {}
 
 variable "team_name" {
   default = "evidence"
@@ -19,29 +15,23 @@ variable "app_language" {
 }
 
 variable "location" {
-  type    = "string"
   default = "UK South"
 }
 
-variable "env" {
-  type = "string"
-}
+variable "env" {}
 
-variable "subscription" {
-  type = "string"
-}
+variable "subscription" {}
 
 variable "ilbIp"{}
 
 variable "tenant_id" {}
 
 variable "jenkins_AAD_objectId" {
-  type = "string"
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
 variable "common_tags" {
-  type = "map"
+  type = map(string)
 }
 ////////////////////////////////////////////////
 //Addtional Vars ///////////////////////////////
@@ -131,13 +121,11 @@ variable "appinsights_instrumentation_key" {
   default     = ""
 }
 variable "appinsights_location" {
-  type        = "string"
   default     = "West Europe"
   description = "Location for Application Insights"
 }
 variable "application_type" {
-  type        = "string"
-  default     = "Web"
+  default     = "web"
   description = "Type of Application Insights (Web/Other)"
 }
 variable "managed_identity_object_id" {
