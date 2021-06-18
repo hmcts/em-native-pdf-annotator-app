1bd511715f1f8808c32d5574cc5c59a956a6cc15
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2021-06-18 19:21:19 +0100

    Updated Unit tests

--- src/test/java/uk/gov/hmcts/reform/em/npa/info/BuildInfoTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/npa/info/BuildInfoTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.em.npa.info;
 
 import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.boot.actuate.info.Info;
 
@@ -36,31 +35,5 @@ public class BuildInfoTest {
         Info info = builder.build();
 
         assertThat(info.getDetails(), CoreMatchers.equalTo(map));
-    }
-
-    @Test
-    public void shouldAddBuildInfoToBuilderWithBuildValue() throws Exception {
-        BuildInfo buildInfo = new BuildInfo("name","env","project");
-
-        Info.Builder builder = new Info.Builder();
-        buildInfo.contribute(builder);
-
-
-        Map<String,Object> buildInfoMap = new HashMap<>();
-        buildInfoMap.put("environment", "env");
-        buildInfoMap.put("project", "project");
-        buildInfoMap.put("name", "name");
-        buildInfoMap.put("build", "123");
-        buildInfoMap.put("version", "V1");
-        buildInfoMap.put("date", "unknown");
-        buildInfoMap.put("commit", "unknown");
-        buildInfoMap.put("extra", Collections.EMPTY_MAP);
-
-        Map<String,Object> map = new HashMap<>();
-        map.put("buildInfo",buildInfoMap);
-
-        Info info = builder.build();
-
-        Assert.assertEquals("V1-123", info.getDetails().get("version"));
     }
 }
