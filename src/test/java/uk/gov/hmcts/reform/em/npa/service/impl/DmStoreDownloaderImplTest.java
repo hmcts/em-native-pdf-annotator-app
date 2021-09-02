3b4c2164b077a58ba4e41d4392ff9b934e071aa1
matt-k1998 <matt.kolakowski@hotmail.com>
2021-09-02 14:47:34 +0100

    Fix failing test: DmStoreDownloaderImplTest

--- src/test/java/uk/gov/hmcts/reform/em/npa/service/impl/DmStoreDownloaderImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/npa/service/impl/DmStoreDownloaderImplTest.java
@@ -1,12 +1,16 @@
 package uk.gov.hmcts.reform.em.npa.service.impl;
 
-import org.junit.Ignore;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
+import okhttp3.mock.MockInterceptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.em.npa.Application;
 import uk.gov.hmcts.reform.em.npa.TestSecurityConfiguration;
 import uk.gov.hmcts.reform.em.npa.service.DmStoreDownloader;
@@ -15,15 +19,32 @@ import uk.gov.hmcts.reform.em.npa.service.exception.DocumentTaskProcessingExcept
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {Application.class, TestSecurityConfiguration.class})
 @Transactional
-@Ignore
-//TODO need to fix this test
 public class DmStoreDownloaderImplTest {
 
     @Autowired
     DmStoreDownloader dmStoreDownloader;
 
+    MockInterceptor interceptor;
+
+    AuthTokenGenerator authTokenGenerator;
+
     @Test(expected = DocumentTaskProcessingException.class)
     public void downloadFile() throws Exception {
+
+        interceptor = new MockInterceptor();
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
+
+        authTokenGenerator = Mockito.mock(AuthTokenGenerator.class);
+
+        dmStoreDownloader = new DmStoreDownloaderImpl(client,
+                authTokenGenerator,
+                "http://localhost:4603",
+                new ObjectMapper());
+
+
         dmStoreDownloader.downloadFile("xxx");
     }
 }
