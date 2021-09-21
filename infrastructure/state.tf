35a67a89e4339dc0f1cd93713ea3e56f3eee8556
rishikrsharma <rishikrsharma@users.noreply.github.com>
2021-09-21 16:22:32 +0100

    EM-4048: terraform provider patch (#629)
    * terraform provider patch
    
    * terraform provider patch

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.0.0"
+      version = "1.6.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
