e61ea98fcdf42db0a38a8ca873587b1b38f88942
matt-k1998 <matt.kolakowski@hotmail.com>
2021-10-14 15:21:02 +0100

    Improve test coverage

---
+++ src/test/java/uk/gov/hmcts/reform/em/npa/rest/errors/BadRequestAlertExceptionTest.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.em.npa.rest.errors;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BadRequestAlertExceptionTest {
+
+    @Autowired
+    BadRequestAlertException badRequestAlertException;
+
+    @Test
+    public void testException() {
+//        Assert.assertEquals();
+
+    }
+}
--- src/test/java/uk/gov/hmcts/reform/em/npa/service/impl/DmStoreDownloaderImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/npa/service/impl/DmStoreDownloaderImplTest.java
@@ -2,6 +2,9 @@ package uk.gov.hmcts.reform.em.npa.service.impl;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,11 +19,15 @@ import uk.gov.hmcts.reform.em.npa.service.exception.DocumentTaskProcessingExcept
 @Transactional
 public class DmStoreDownloaderImplTest {
 
+    @Mock
+    Logger log;
+
     @Autowired
     DmStoreDownloader dmStoreDownloader;
 
     @Test(expected = DocumentTaskProcessingException.class)
     public void downloadFile() throws Exception {
         dmStoreDownloader.downloadFile("xxx");
+        Mockito.when(log.isInfoEnabled()).thenReturn(true);
     }
 }
