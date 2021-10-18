200bd2f402fc66965e415af5ee073f8e6a32282e
matt-k1998 <matt.kolakowski@hotmail.com>
2021-10-18 16:23:10 +0100

    Update the classes: RedactionResource, BadRequestAlertException

--- src/main/java/uk/gov/hmcts/reform/em/npa/rest/errors/BadRequestAlertException.java
+++ src/main/java/uk/gov/hmcts/reform/em/npa/rest/errors/BadRequestAlertException.java
@@ -20,7 +20,15 @@ public class BadRequestAlertException extends AbstractThrowableProblem {
     }
 
     public BadRequestAlertException(URI type, String defaultMessage, String entityName, String errorKey) {
-        super(type, defaultMessage, Status.BAD_REQUEST, null, null, null, getAlertParameters(entityName, errorKey));
+        super(
+                type,
+                defaultMessage,
+                Status.BAD_REQUEST,
+                null,
+                null,
+                null,
+                getAlertParameters(entityName, errorKey)
+        );
         this.entityName = entityName;
         this.errorKey = errorKey;
     }
