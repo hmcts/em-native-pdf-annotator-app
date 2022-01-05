0af0c0546f58cd2d19c246eec3b09f68a9912374
matt-k1998 <matt.kolakowski@hotmail.com>
2022-01-05 11:06:53 +0000

    Add a test class for the FieldErrorVM

---
+++ src/test/java/uk/gov/hmcts/reform/em/npa/rest/errors/FieldErrorVMTest.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.em.npa.rest.errors;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldErrorVMTest {
+
+    FieldErrorVM fieldErrorVM = new FieldErrorVM("testObjectName", "testField", "testMessage");
+
+    @Test
+    void getObjectName() {
+        assertSame("testObjectName", fieldErrorVM.getObjectName());
+    }
+
+    @Test
+    void getField() {
+        assertSame("testField", fieldErrorVM.getField());
+    }
+
+    @Test
+    void getMessage() {
+        assertSame("testMessage", fieldErrorVM.getMessage());
+    }
+}
