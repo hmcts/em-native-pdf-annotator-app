f2fdbc271db6ecd09f0d9e65b6b1a9fb224543b1
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2022-03-09 08:07:24 +0000

    Upgraded SpringBoot version to 2.6.4

--- src/main/java/uk/gov/hmcts/reform/em/npa/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/npa/config/SwaggerConfiguration.java
@@ -1,66 +1,20 @@
 package uk.gov.hmcts.reform.em.npa.config;
 
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Arrays;
 
 @Configuration
-@EnableSwagger2
 @ComponentScan("uk.gov.hmcts.reform.em.npa.rest")
 public class SwaggerConfiguration {
 
-    private final static String apiVersion = "1.0.1";
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.regex("/api(.*)"))
-                .build()
-                .apiInfo(apiInfo())
-                .globalOperationParameters(Arrays.asList(
-                    headerAuthorization(),
-                    headerServiceAuthorization()));
-    }
-
-    private Parameter headerAuthorization() {
-        return new ParameterBuilder()
-            .name("Authorization")
-            .description("Keyword `Bearer` followed by a valid IDAM user token")
-            .modelRef(new ModelRef("string"))
-            .parameterType("header")
-            .required(true)
-            .build();
-    }
-
-    private Parameter headerServiceAuthorization() {
-        return new ParameterBuilder()
-            .name("ServiceAuthorization")
-            .description("Valid Service-to-Service JWT token for a whitelisted micro-service")
-            .modelRef(new ModelRef("string"))
-            .parameterType("header")
-            .required(true)
-            .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("EM Native PDF Annotator App")
-                .description("API to burn annotations onto a PDF and facilitate Document Redaction")
-                .version(apiVersion)
-                .build();
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(
+                        new Info().title("EM Native PDF Annotator App")
+                                .description("API to burn annotations onto a PDF and facilitate Document Redaction")
+                                .version("v1.0.1")
+                );
     }
 }
