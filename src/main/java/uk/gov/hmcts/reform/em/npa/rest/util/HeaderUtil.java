bb6a71990c7394cb2fff6e1d9a22e966bcb50b0c
matt-k1998 <matt.kolakowski@hotmail.com>
2022-04-28 15:13:48 +0100

    add test class for DmStoreDownloaderImpl

--- src/main/java/uk/gov/hmcts/reform/em/npa/rest/util/HeaderUtil.java
+++ src/main/java/uk/gov/hmcts/reform/em/npa/rest/util/HeaderUtil.java
@@ -1,15 +1,11 @@
 package uk.gov.hmcts.reform.em.npa.rest.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 
 /**
  * Utility class for HTTP headers creation.
  */
 public final class HeaderUtil {
-
-    private static final Logger log = LoggerFactory.getLogger(HeaderUtil.class);
 
     private static final String APPLICATION_NAME = "npaApp";
 
