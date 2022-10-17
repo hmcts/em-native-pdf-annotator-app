f31caf2e9c0bb0a9d1762d8d0a710f2386fa14de
mdayican <mustafa.dayican@hmcts.net>
2022-10-17 15:20:57 +0100

    EM-4666 use debug level

--- src/main/java/uk/gov/hmcts/reform/em/npa/Application.java
+++ src/main/java/uk/gov/hmcts/reform/em/npa/Application.java
@@ -2,12 +2,10 @@ package uk.gov.hmcts.reform.em.npa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
