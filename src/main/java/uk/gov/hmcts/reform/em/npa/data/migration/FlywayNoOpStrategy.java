1b8d3d651a4066cd8aa16ca57ef510afbc52a798
SomeshRA <somesh.acharya1@hmcts.net>
2022-11-21 14:28:31 +0000

    EM-4813: Logging to understand flyway strategy

--- src/main/java/uk/gov/hmcts/reform/em/npa/data/migration/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/em/npa/data/migration/FlywayNoOpStrategy.java
@@ -1,15 +1,21 @@
 package uk.gov.hmcts.reform.em.npa.data.migration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 import java.util.stream.Stream;
 
+@Slf4j
 public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
 
     @Override
     public void migrate(Flyway flyway) {
         Stream.of(flyway.info().all())
+                .peek(info ->
+                        log.info("flyway migration info {} and state {}",
+                                info,
+                                info.getState()))
                 .filter(info -> !info.getState().isApplied())
                 .findFirst()
                 .ifPresent(info -> {
