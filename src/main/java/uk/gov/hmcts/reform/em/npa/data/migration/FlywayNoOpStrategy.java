2cf35064f4c19ec4958b386c91fc87d60c406776
SomeshRA <69344273+SomeshRA@users.noreply.github.com>
2022-11-24 15:46:17 +0000

    EM-4813:  Updating flyway db to 9.8.2 (#1171)
    * EM-4813: Logging to understand flyway strategy
    
    * EM-4813: Logging to understand flyway strategy
    
    * EM-4813: Logging to understand flyway strategy
    
    * EM-4813: Logging to understand flyway strategy
    
    * EM-4813: Logging 9.8.2 to understand flyway strategy
    
    * EM-4813: Setting Preview migration to same as prod
    
    * EM-4813: Update flyway to 8.5.13
    
    * EM-4813: Update flyway to 9.8.2
    
    * EM-4813: Reverting to master
    
    * EM-4813: Checking the imigration with flyway 9.8.2
    
    * EM-4813: Reverting to master
    
    * EM-4813: Reverting to master
    
    * EM-4813: test
    
    * EM-4813: test
    
    * EM-4813: reverted testing changes
    
    * EM-4813: Resolving conflicts
    
    Co-authored-by: yogesh-hullatti <yogesh.hullatti@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/em/npa/data/migration/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/em/npa/data/migration/FlywayNoOpStrategy.java
@@ -1,16 +1,24 @@
 package uk.gov.hmcts.reform.em.npa.data.migration;
 
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 import java.util.stream.Stream;
+
+import static org.flywaydb.core.api.MigrationState.BASELINE;
+import static org.flywaydb.core.api.MigrationState.BASELINE_IGNORED;
+
 
 public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
 
     @Override
     public void migrate(Flyway flyway) {
         Stream.of(flyway.info().all())
-                .filter(info -> !info.getState().isApplied())
+                .filter(info -> !BASELINE.equals(info.getState())
+                        && !BASELINE_IGNORED.equals(info.getState())
+                        && !info.getState().isApplied())
                 .findFirst()
                 .ifPresent(info -> {
                     throw new PendingMigrationScriptException(info.getScript());
