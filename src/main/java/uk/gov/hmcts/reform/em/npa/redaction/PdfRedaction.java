49937e086d37e52ccef50c887abff1d02b5de53b
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2022-11-25 21:40:46 +0000

    Reduced dpi to 200

--- src/main/java/uk/gov/hmcts/reform/em/npa/redaction/PdfRedaction.java
+++ src/main/java/uk/gov/hmcts/reform/em/npa/redaction/PdfRedaction.java
@@ -104,7 +104,7 @@ public class PdfRedaction {
      * @throws IOException
      */
     private File transformToImage(PDFRenderer pdfRenderer, int pageNumber) throws IOException {
-        BufferedImage img = pdfRenderer.renderImageWithDPI(pageNumber, 300, ImageType.ARGB);
+        BufferedImage img = pdfRenderer.renderImageWithDPI(pageNumber, 200, ImageType.ARGB);
         final File alteredImage = File.createTempFile("altered", ".png");
         ImageIO.write(img, "png", alteredImage);
         return alteredImage;
