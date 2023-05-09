ebea6018536b921f29e4cebfda4569c5bea7daa9
mdayican <mustafa.dayican@hmcts.net>
2023-05-09 17:53:37 +0100

    try to fix smoke test-3

--- src/aat/java/uk/gov/hmcts/reform/em/npa/smoke/SmokeTest.java
+++ src/aat/java/uk/gov/hmcts/reform/em/npa/smoke/SmokeTest.java
@@ -1,12 +1,10 @@
 package uk.gov.hmcts.reform.em.npa.smoke;
 
-import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.WithTag;
 import net.thucydides.core.annotations.WithTags;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -17,7 +15,6 @@ import java.util.Map;
 
 @SpringBootTest(classes = {EmTestConfig.class, Application.class})
 @TestPropertySource(value = "classpath:application.yml")
-@RunWith(SpringIntegrationSerenityRunner.class)
 @WithTags({@WithTag("testType:Smoke")})
 public class SmokeTest {
 
