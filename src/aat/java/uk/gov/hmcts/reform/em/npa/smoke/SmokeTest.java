df0aeb5e3274f321b3f8aff873caed9756b4dd91
mdayican <mustafa.dayican@hmcts.net>
2023-05-09 22:31:59 +0100

    revert back and disable batch schema

--- src/aat/java/uk/gov/hmcts/reform/em/npa/smoke/SmokeTest.java
+++ src/aat/java/uk/gov/hmcts/reform/em/npa/smoke/SmokeTest.java
@@ -1,23 +1,32 @@
 package uk.gov.hmcts.reform.em.npa.smoke;
 
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.WithTag;
+import net.thucydides.core.annotations.WithTags;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.em.EmTestConfig;
+import uk.gov.hmcts.reform.em.npa.Application;
+import uk.gov.hmcts.reform.em.npa.testutil.TestUtil;
 
 import java.util.Map;
 
-@ExtendWith(SpringExtension.class)
-@TestPropertySource("classpath:application.yaml")
+@SpringBootTest(classes = {TestUtil.class, EmTestConfig.class, Application.class})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@TestPropertySource(value = "classpath:application.yml")
+@RunWith(SpringIntegrationSerenityRunner.class)
+@WithTags({@WithTag("testType:Smoke")})
 public class SmokeTest {
 
     private static final String MESSAGE = "Welcome to Native PDF Annotator API!";
-    private final Logger log = LoggerFactory.getLogger(SmokeTest.class);
 
     @Value("${test.url}")
     private String testUrl;
@@ -27,23 +36,19 @@ public class SmokeTest {
 
         SerenityRest.useRelaxedHTTPSValidation();
 
-        log.info("SmokeTest testUrl ===> {}", testUrl);
-        try {
-            Map responseMap =
-                    SerenityRest
-                            .given()
-                            .baseUri(testUrl)
-                            .get("/")
-                            .then()
-                            .statusCode(200)
-                            .extract()
-                            .body()
-                            .as(Map.class);
+        Map responseMap =
+                SerenityRest
+                        .given()
+                        .baseUri(testUrl)
+                        .get("/")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(Map.class);
 
-            Assert.assertEquals(1, responseMap.size());
-            Assert.assertEquals(MESSAGE, responseMap.get("message"));
-        } catch (Exception ex) {
-            log.error("SmokeTest error:{}", ex.getMessage());
-        }
+        Assert.assertEquals(1, responseMap.size());
+        Assert.assertEquals(MESSAGE, responseMap.get("message"));
+
     }
 }
