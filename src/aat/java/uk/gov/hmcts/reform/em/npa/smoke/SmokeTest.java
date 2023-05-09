55017bcac1401c78ead40a36ee2893755c22249d
mdayican <mustafa.dayican@hmcts.net>
2023-05-09 22:06:04 +0100

    try to log

--- src/aat/java/uk/gov/hmcts/reform/em/npa/smoke/SmokeTest.java
+++ src/aat/java/uk/gov/hmcts/reform/em/npa/smoke/SmokeTest.java
@@ -1,12 +1,11 @@
 package uk.gov.hmcts.reform.em.npa.smoke;
 
-import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
-import net.thucydides.core.annotations.WithTag;
-import net.thucydides.core.annotations.WithTags;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -18,6 +17,7 @@ import java.util.Map;
 public class SmokeTest {
 
     private static final String MESSAGE = "Welcome to Native PDF Annotator API!";
+    private final Logger log = LoggerFactory.getLogger(SmokeTest.class);
 
     @Value("${test.url}")
     private String testUrl;
@@ -27,19 +27,23 @@ public class SmokeTest {
 
         SerenityRest.useRelaxedHTTPSValidation();
 
-        Map responseMap =
-                SerenityRest
-                        .given()
-                        .baseUri(testUrl)
-                        .get("/")
-                        .then()
-                        .statusCode(200)
-                        .extract()
-                        .body()
-                        .as(Map.class);
+        log.info("SmokeTest testUrl ===> {}", testUrl);
+        try {
+            Map responseMap =
+                    SerenityRest
+                            .given()
+                            .baseUri(testUrl)
+                            .get("/")
+                            .then()
+                            .statusCode(200)
+                            .extract()
+                            .body()
+                            .as(Map.class);
 
-        Assert.assertEquals(1, responseMap.size());
-        Assert.assertEquals(MESSAGE, responseMap.get("message"));
-
+            Assert.assertEquals(1, responseMap.size());
+            Assert.assertEquals(MESSAGE, responseMap.get("message"));
+        } catch (Exception ex) {
+            log.error("SmokeTest error:{}", ex.getMessage());
+        }
     }
 }
