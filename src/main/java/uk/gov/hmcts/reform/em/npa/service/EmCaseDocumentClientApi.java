95c59b381df2bda1747c83db4f18f6a0b172991c
mdayican <mustafa.dayican@hmcts.net>
2023-05-12 15:17:38 +0100

    remove unused class

--- src/main/java/uk/gov/hmcts/reform/em/npa/service/EmCaseDocumentClientApi.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.em.npa.service;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClientApi;
-
-@FeignClient(configuration = CaseDocumentClientApi.class)
-public interface EmCaseDocumentClientApi {
-}
