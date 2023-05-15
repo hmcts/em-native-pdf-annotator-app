aacc98a2f8440d8e5439de9215969847b7b6c7ed
MUSTAFA <mdayican@gmail.com>
2023-05-15 15:32:18 +0100

    Em-5116 remove spring boot tests and refactor test (#1441)

--- src/test/java/uk/gov/hmcts/reform/em/npa/service/impl/DmStoreDownloaderImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/npa/service/impl/DmStoreDownloaderImplTest.java
@@ -6,27 +6,16 @@ import okhttp3.mock.MockInterceptor;
 import okhttp3.mock.Rule;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
-import uk.gov.hmcts.reform.em.npa.Application;
-import uk.gov.hmcts.reform.em.npa.TestSecurityConfiguration;
 import uk.gov.hmcts.reform.em.npa.service.DmStoreDownloader;
 import uk.gov.hmcts.reform.em.npa.service.exception.DocumentTaskProcessingException;
 
 import java.io.IOException;
 import java.util.UUID;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = {Application.class, TestSecurityConfiguration.class})
-@Transactional
 public class DmStoreDownloaderImplTest {
 
-    @Autowired
     DmStoreDownloader dmStoreDownloader;
 
     AuthTokenGenerator authTokenGenerator;
