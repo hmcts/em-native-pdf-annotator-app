36a5f9fb9447cd02a86b8f4bb462047aa2ad9cab
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2023-08-23 17:27:07 +0100

    Added in FLYWAY_POSTGRESQL_TRANSACTIONAL_LOCK

--- src/main/resources/db/migration/V2__add_index_redid_docid_createdby.sql
+++ src/main/resources/db/migration/V2__add_index_redid_docid_createdby.sql
@@ -1,1 +1,1 @@
-CREATE INDEX IF NOT EXISTS redactionid_documentid_createdby ON redaction (redaction_id,document_id, created_by);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS redactionid_documentid_createdby ON redaction (redaction_id,document_id, created_by);
