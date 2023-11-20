e292ee2adc40693e0dae720c0da72f69667582ea
lars lunde <lars.lunde@hmcts.net>
2023-11-20 10:35:01 +0000

    fixing import statements

--- src/main/java/uk/gov/hmcts/reform/em/npa/batch/EntityValueProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/em/npa/batch/EntityValueProcessor.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.npa.Propagation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
