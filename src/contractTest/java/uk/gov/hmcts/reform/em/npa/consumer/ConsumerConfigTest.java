989b2bd83ad20b8d6eced682fa5867bc3d9b55ab
lars lunde <lars.lunde@hmcts.net>
2023-12-13 09:39:28 +0000

    removed empty consumer/provider tests to prevent any chance of issues arising in prod, will re-add in local

--- src/contractTest/java/uk/gov/hmcts/reform/em/npa/consumer/ConsumerConfigTest.java
+++
@@ -1,4 +0,0 @@
-package uk.gov.hmcts.reform.em.npa.consumer;
-
-public class ConsumerConfigTest {
-}
--- src/contractTest/java/uk/gov/hmcts/reform/em/npa/provider/ProviderConfigTest.java
+++
@@ -1,5 +0,0 @@
-package uk.gov.hmcts.reform.em.npa.provider;
-
-public class ProviderConfigTest {
-
-}
