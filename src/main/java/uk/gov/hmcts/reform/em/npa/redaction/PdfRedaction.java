55167d982bdf471665cb582012e4575bfe1c0156
mdayican <mdayican@gmail.com>
2025-02-19 14:21:16 +0000

    EM-6414 info level log

--- src/main/java/uk/gov/hmcts/reform/em/npa/redaction/PdfRedaction.java
+++ src/main/java/uk/gov/hmcts/reform/em/npa/redaction/PdfRedaction.java
@@ -91,14 +91,16 @@ public class PdfRedaction {
                 - pixelToPointConversion(rectangle.getHeight()),
             pixelToPointConversion(rectangle.getWidth()),
             pixelToPointConversion(rectangle.getHeight()));
-        log.debug("Pagesize height {}, width:{}, lowerLeftX:{}, LowerLeftY:{}, upperRightX:{}, upperRightY:{}",
+        log.info("Redaction rectangle,
+                 Pagesize height {}, width:{}, lowerLeftX:{}, LowerLeftY:{}, upperRightX:{}, upperRightY:{}",
                 pageSize.getHeight(),
                 pageSize.getWidth(),
                 pageSize.getLowerLeftX(),
                 pageSize.getLowerLeftY(),
                 pageSize.getUpperRightX(),
                 pageSize.getUpperRightY());
-        log.debug("x:{},y:{},width:{},height:{}", rec.getX(), rec.getY(), rec.getWidth(), rec.getHeight());
+        log.info("Redaction rectangle,
+                 x:{},y:{},width:{},height:{}", rec.getX(), rec.getY(), rec.getWidth(), rec.getHeight());
         return rec;
 
     }
