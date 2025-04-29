68d797136ecdf426e3c46c102d0f481908d6ccfe
mdayican <mdayican@gmail.com>
2025-04-29 14:48:14 +0100

    EM-6512 add pact test

--- src/contractTest/java/uk/gov/hmcts/reform/em/npa/NpaPactProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/npa/NpaPactProviderTest.java
@@ -6,6 +6,7 @@ import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
@@ -13,10 +14,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.reform.em.npa.rest.MarkUpResource;
+import uk.gov.hmcts.reform.em.npa.service.MarkUpService;
+import uk.gov.hmcts.reform.em.npa.service.dto.redaction.RectangleDTO;
+import uk.gov.hmcts.reform.em.npa.service.dto.redaction.RedactionDTO;
+
+import java.util.Set;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 
 @Provider("em_npa_provider")
@@ -25,13 +39,27 @@ import org.springframework.test.web.servlet.MockMvc;
         port = "${PACT_BROKER_PORT:80}",
         consumerVersionSelectors = {@VersionSelector(tag = "master")})
 @IgnoreNoPactsToVerify
-@AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = Application.class)
+@WebMvcTest(value = MarkUpResource.class,
+        excludeAutoConfiguration = {SecurityAutoConfiguration.class, OAuth2ClientAutoConfiguration.class})
+@AutoConfigureMockMvc(addFilters = false)
 public class NpaPactProviderTest {
 
+
+    private static final UUID REDACTION_ID = UUID.fromString("4c34ba4a-585a-407d-aa78-3f86f3171cdd");
+    private static final UUID DOCUMENT_ID = UUID.fromString("f2cc4d79-d0f3-4b43-affe-535516370cdd");
+    private static final UUID RECTANGLE_ID = UUID.fromString("c04b807f-8352-4bfc-95b5-cecd072b7aba");
+
+    // Static coordinates
+    private static final Double X = 10.5;
+    private static final Double Y = 20.5;
+    private static final Double WIDTH = 100.0;
+    private static final Double HEIGHT = 200.0;
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean
+    private MarkUpService markUpService;
 
     @TestTemplate
     @ExtendWith(PactVerificationInvocationContextProvider.class)
@@ -48,5 +76,28 @@ public class NpaPactProviderTest {
             context.setTarget(new MockMvcTestTarget(mockMvc));
         }
     }
+
+    @State("A valid RedactionDTO exists")
+    public void setupValidRedactionDTO() {
+        when(markUpService.save(any(RedactionDTO.class))).thenAnswer(invocation -> {
+            RedactionDTO request = invocation.getArgument(0); // Get the request object
+
+            RectangleDTO rectangle = new RectangleDTO();
+            rectangle.setId(request.getRectangles().iterator().next().getId()); // Use request's rectangle ID
+            rectangle.setX(X);
+            rectangle.setY(Y);
+            rectangle.setWidth(WIDTH);
+            rectangle.setHeight(HEIGHT);
+
+            RedactionDTO response = new RedactionDTO();
+            response.setRedactionId(request.getRedactionId()); // Echo request's redaction ID
+            response.setDocumentId(request.getDocumentId());   // Echo request's document ID
+            response.setPage(1);
+            response.setRectangles(Set.of(rectangle));
+
+            return response;
+        });
+    }
+
 
 }
