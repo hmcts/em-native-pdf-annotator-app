e0db038eaff25e9a800631f001508084e846ef00
michaeljacob <Michael.Jacob@HMCTS.NET>
2025-09-22 11:20:11 +0100

    remove redundant field

--- src/contractTest/java/uk/gov/hmcts/reform/em/npa/consumer/NpaPactRedactionConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/npa/consumer/NpaPactRedactionConsumerTest.java
@@ -54,7 +54,6 @@ public class NpaPactRedactionConsumerTest {
                 .body(LambdaDsl.newJsonBody(root -> {
                     root.stringType("caseId", "123456789");
                     root.uuid("documentId", DOCUMENT_ID);
-                    root.stringType("redactedFileName", "document-redacted.pdf");
                     root.minArrayLike("redactions", 1, redaction -> {
                         redaction.uuid("redactionId", REDACTION_ID);
                         redaction.uuid("documentId", DOCUMENT_ID);
@@ -77,7 +76,6 @@ public class NpaPactRedactionConsumerTest {
             {
               "caseId": "123456789",
               "documentId": "%s",
-              "redactedFileName": "document-redacted.pdf",
               "redactions": [
                 {
                   "redactionId": "%s",
