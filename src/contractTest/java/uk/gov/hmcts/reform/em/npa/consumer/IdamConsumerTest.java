84cc4e76678b274d3af01b6c540e9552adec18df
michaeljacob <Michael.Jacob@HMCTS.NET>
2025-09-22 16:02:23 +0100

    update pact config

--- src/contractTest/java/uk/gov/hmcts/reform/em/npa/consumer/IdamConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/npa/consumer/IdamConsumerTest.java
@@ -10,10 +10,10 @@ import au.com.dius.pact.core.model.PactSpecVersion;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import com.google.common.collect.Maps;
+import io.restassured.http.ContentType;
 import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.rest.SerenityRest;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,7 +26,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.Map;
 import java.util.TreeMap;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.hamcrest.Matchers.notNullValue;
 
 @Slf4j
 @ExtendWith(PactConsumerTestExt.class)
@@ -36,6 +38,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class IdamConsumerTest {
 
     private static final String IDAM_DETAILS_URL = "/o/userinfo";
+    private static final String EXAMPLE_AUTH_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJraWQiOiJiL082T3ZWdjEre";
 
     @Pact(provider = "Idam_api", consumer = "em_npa_api")
     public RequestResponsePact executeGetUserInfoAndGet200(PactDslWithProvider builder) {
@@ -58,7 +61,7 @@ public class IdamConsumerTest {
                 .given("I have obtained an access_token as a user", params)
                 .uponReceiving("Provider returns user info to EM Native Pdf Annotator API")
                 .path(IDAM_DETAILS_URL)
-                .headers("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJraWQiOiJiL082T3ZWdjEre")
+                .headers("Authorization", EXAMPLE_AUTH_TOKEN)
                 .method(HttpMethod.GET.toString())
                 .willRespondWith()
                 .status(HttpStatus.OK.value())
@@ -69,30 +72,26 @@ public class IdamConsumerTest {
 
     @Test
     @PactTestFor(pactMethod = "executeGetUserInfoAndGet200")
-    public void should_get_user_details_with_access_token(MockServer mockServer) throws JSONException {
+    public void shouldGetUserDetailsWithAccessToken(MockServer mockServer) throws JSONException {
 
         Map<String, String> headers = Maps.newHashMap();
-        headers.put(HttpHeaders.AUTHORIZATION, "Bearer eyJ0eXAiOiJKV1QiLCJraWQiOiJiL082T3ZWdjEre");
+        headers.put(HttpHeaders.AUTHORIZATION, EXAMPLE_AUTH_TOKEN);
         headers.put(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 
-        String detailsResponseBody =
-                SerenityRest
-                        .given()
-                        .headers(headers)
-                        .when()
-                        .get(mockServer.getUrl() + IDAM_DETAILS_URL)
-                        .then()
-                        .statusCode(200)
-                        .and()
-                        .extract()
-                        .body()
-                        .asString();
-
-        JSONObject response = new JSONObject(detailsResponseBody);
-
-        assertThat(detailsResponseBody).isNotNull();
-        assertThat(response).hasNoNullFieldsOrProperties();
-        assertThat(response.getString("uid")).isNotBlank();
+        SerenityRest
+            .given()
+            .header(HttpHeaders.AUTHORIZATION, EXAMPLE_AUTH_TOKEN)
+            .contentType(ContentType.JSON)
+            .when()
+            .get(mockServer.getUrl() + IDAM_DETAILS_URL)
+            .then()
+            .statusCode(200)
+            .and()
+            .body(notNullValue())
+            .body("uid", not(emptyOrNullString()))
+            .body("email", notNullValue())
+            .body("forename", notNullValue())
+            .body("surname", notNullValue());
     }
 
     private DslPart createUserInfoResponse() {
--- src/contractTest/java/uk/gov/hmcts/reform/em/npa/consumer/NpaPactConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/npa/consumer/NpaPactConsumerTest.java
@@ -2,7 +2,9 @@ package uk.gov.hmcts.reform.em.npa.consumer;
 
 
 import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.DslPart;
 import au.com.dius.pact.consumer.dsl.LambdaDsl;
+import au.com.dius.pact.consumer.dsl.LambdaDslObject;
 import au.com.dius.pact.consumer.dsl.PactBuilder;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
@@ -28,11 +30,16 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 @ExtendWith(PactConsumerTestExt.class)
 @ExtendWith(SpringExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+// S1192: Using string literals for JSON/request field names intentionally to keep structure clear in tests.
+@SuppressWarnings("squid:S1192")
 public class NpaPactConsumerTest {
 
     public static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
     public static final String AUTH_TOKEN = "Bearer someAuthorizationToken";
     public static final String SERVICE_AUTH_TOKEN = "Bearer someServiceAuthorizationToken";
+
+    private static final String NPA_PROVIDER = "native_pdf_annotator_api_provider";
+    private static final String MARKUPS_API_PATH = "/api/markups";
 
     // Static UUIDs from provided list
     private static final UUID REDACTION_ID = UUID.fromString("4c34ba4a-585a-407d-aa78-3f86f3171cdd");
@@ -41,268 +48,193 @@ public class NpaPactConsumerTest {
 
     public Map<String, String> getHeaders() {
         return Map.of(
-                SERVICE_AUTHORIZATION, SERVICE_AUTH_TOKEN,
-                AUTHORIZATION, AUTH_TOKEN,
-                "Content-Type", "application/json"
+            SERVICE_AUTHORIZATION, SERVICE_AUTH_TOKEN,
+            AUTHORIZATION, AUTH_TOKEN,
+            "Content-Type", "application/json"
         );
     }
 
-    @Pact(consumer = "em_npa_api", provider = "native_pdf_annotator_api_provider")
+    @Pact(consumer = "em_npa_api", provider = NPA_PROVIDER)
     public V4Pact createMarkUpPact(PactBuilder builder) {
         return builder
-                .usingLegacyDsl()
-                .given("A valid RedactionDTO exists")
-                .uponReceiving("POST request to create RedactionDTO")
-                .path("/api/markups")
-                .method("POST")
-                .headers(getHeaders())
-                .body(LambdaDsl.newJsonBody(root -> {
-                    root.uuid("redactionId", REDACTION_ID); // Set expected UUID
-                    root.uuid("documentId", DOCUMENT_ID);   // Set expected UUID
-                    root.integerType("page", 1);
-                    root.minArrayLike("rectangles", 1, rect -> {
-                        rect.uuid("id", RECTANGLE_ID); // Set expected UUID
-                        rect.numberType("x", 10.5);
-                        rect.numberType("y", 20.5);
-                        rect.numberType("width", 100.0);
-                        rect.numberType("height", 200.0);
-                    });
-                }).build())
-                .willRespondWith()
-                .status(201)
-                .headers(Map.of("Content-Type", MediaType.APPLICATION_JSON_VALUE))
-                .body(LambdaDsl.newJsonBody(root -> {
-                    root.uuid("redactionId", REDACTION_ID); // Match request ID
-                    root.uuid("documentId", DOCUMENT_ID);   // Match request ID
-                    root.integerType("page", 1);
-                    root.minArrayLike("rectangles", 1, rect -> {
-                        rect.uuid("id", RECTANGLE_ID); // Match request ID
-                        rect.numberType("x", 10.5);
-                        rect.numberType("y", 20.5);
-                        rect.numberType("width", 100.0);
-                        rect.numberType("height", 200.0);
-                    });
-                }).build())
-                .toPact(V4Pact.class);
+            .usingLegacyDsl()
+            .given("A valid RedactionDTO exists")
+            .uponReceiving("POST request to create RedactionDTO")
+            .path(MARKUPS_API_PATH)
+            .method("POST")
+            .headers(getHeaders())
+            .body(createRedactionDtoDsl())
+            .willRespondWith()
+            .status(HttpStatus.CREATED.value())
+            .headers(Map.of("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+            .body(createRedactionDtoDsl())
+            .toPact(V4Pact.class);
     }
-
 
     @Test
     @PactTestFor(pactMethod = "createMarkUpPact")
     void testCreateMarkUp(MockServer mockServer) {
-
-
-        String requestBody = String.format("""
-                {
-                  "redactionId": "%s",
-                  "documentId": "%s",
-                  "page": 1,
-                  "rectangles": [
-                    {
-                      "id": "%s",
-                      "x": 10.5,
-                      "y": 20.5,
-                      "width": 100.0,
-                      "height": 200.0
-                    }
-                  ]
-                }
-                """, REDACTION_ID, DOCUMENT_ID, RECTANGLE_ID);
-
         SerenityRest
-                .given()
-                .headers(getHeaders())
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(requestBody)
-                .post(mockServer.getUrl() + "/api/markups")
-                .then()
-                .statusCode(HttpStatus.CREATED.value())
-                .body("redactionId", equalTo(REDACTION_ID.toString()))  // Convert UUID to string
-                .body("documentId", equalTo(DOCUMENT_ID.toString()))
-                .body("rectangles[0].id", equalTo(RECTANGLE_ID.toString()));
+            .given()
+            .headers(getHeaders())
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(createRedactionDtoDsl().getBody().toString())
+            .post(mockServer.getUrl() + MARKUPS_API_PATH)
+            .then()
+            .statusCode(HttpStatus.CREATED.value())
+            .body("redactionId", equalTo(REDACTION_ID.toString()))
+            .body("documentId", equalTo(DOCUMENT_ID.toString()))
+            .body("rectangles[0].id", equalTo(RECTANGLE_ID.toString()));
     }
 
-    @Pact(consumer = "em_npa_api", provider = "native_pdf_annotator_api_provider")
+    @Pact(consumer = "em_npa_api", provider = NPA_PROVIDER)
     public V4Pact getMarkupsPact(PactBuilder builder) {
         return builder
-                .usingLegacyDsl()
-                .given("Markups exist for document " + DOCUMENT_ID)
-                .uponReceiving("GET request for markups by document ID")
-                .path("/api/markups/" + DOCUMENT_ID)
-                .method("GET")
-                .headers(getHeaders())
-                .willRespondWith()
-                .status(200)
-                .headers(Map.of("Content-Type", MediaType.APPLICATION_JSON_VALUE))
-                .body(newJsonArray(array ->
-                        array.object(markup -> {
-                            markup.uuid("redactionId", REDACTION_ID);
-                            markup.uuid("documentId", DOCUMENT_ID);
-                            markup.integerType("page", 1);
-                            markup.array("rectangles", rectangles ->
-                                    rectangles.object(rect -> {
-                                        rect.uuid("id", RECTANGLE_ID);
-                                        rect.numberType("x", 10.5);
-                                        rect.numberType("y", 20.5);
-                                        rect.numberType("width", 100.0);
-                                        rect.numberType("height", 200.0);
-                                    })
-                            );
-                        }).build()
-                ).build())
-                .toPact(V4Pact.class);
+            .usingLegacyDsl()
+            .given("Markups exist for document " + DOCUMENT_ID)
+            .uponReceiving("GET request for markups by document ID")
+            .path(MARKUPS_API_PATH + "/" + DOCUMENT_ID)
+            .method("GET")
+            .headers(getHeaders())
+            .willRespondWith()
+            .status(HttpStatus.OK.value())
+            .headers(Map.of("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+            .body(createRedactionDtoArrayDsl())
+            .toPact(V4Pact.class);
     }
 
     @Test
     @PactTestFor(pactMethod = "getMarkupsPact")
     void testGetMarkupsByDocumentId(MockServer mockServer) {
         SerenityRest
-                .given()
-                .headers(getHeaders())
-                .get(mockServer.getUrl() + "/api/markups/" + DOCUMENT_ID)
-                .then()
-                .statusCode(HttpStatus.OK.value())
-                .body("[0].redactionId", equalTo(REDACTION_ID.toString()))
-                .body("[0].documentId", equalTo(DOCUMENT_ID.toString()))
-                .body("[0].rectangles[0].id", equalTo(RECTANGLE_ID.toString()))
-                .body("[0].rectangles[0].x", equalTo(10.5f));
+            .given()
+            .headers(getHeaders())
+            .get(mockServer.getUrl() + MARKUPS_API_PATH + "/" + DOCUMENT_ID)
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .body("[0].redactionId", equalTo(REDACTION_ID.toString()))
+            .body("[0].documentId", equalTo(DOCUMENT_ID.toString()))
+            .body("[0].rectangles[0].id", equalTo(RECTANGLE_ID.toString()))
+            .body("[0].rectangles[0].x", equalTo(10.5f));
     }
 
-    @Pact(consumer = "em_npa_api", provider = "native_pdf_annotator_api_provider")
+    @Pact(consumer = "em_npa_api", provider = NPA_PROVIDER)
     public V4Pact updateMarkUpPact(PactBuilder builder) {
         return builder
-                .usingLegacyDsl()
-                .given("A valid RedactionDTO with ID exists")
-                .uponReceiving("PUT request to update RedactionDTO")
-                .path("/api/markups")
-                .method("PUT")
-                .headers(getHeaders())
-                .body(LambdaDsl.newJsonBody(root -> {
-                    root.uuid("redactionId", REDACTION_ID);
-                    root.uuid("documentId", DOCUMENT_ID);
-                    root.integerType("page", 1);
-                    root.minArrayLike("rectangles", 1, rect -> {
-                        rect.uuid("id", RECTANGLE_ID);
-                        rect.numberType("x", 10.5);
-                        rect.numberType("y", 20.5);
-                        rect.numberType("width", 100.0);
-                        rect.numberType("height", 200.0);
-                    });
-                }).build())
-                .willRespondWith()
-                .status(200)
-                .headers(Map.of("Content-Type", MediaType.APPLICATION_JSON_VALUE))
-                .body(LambdaDsl.newJsonBody(root -> {
-                    root.uuid("redactionId", REDACTION_ID);
-                    root.uuid("documentId", DOCUMENT_ID);
-                    root.integerType("page", 1);
-                    root.minArrayLike("rectangles", 1, rect -> {
-                        rect.uuid("id", RECTANGLE_ID);
-                        rect.numberType("x", 10.5);
-                        rect.numberType("y", 20.5);
-                        rect.numberType("width", 100.0);
-                        rect.numberType("height", 200.0);
-                    });
-                }).build())
-                .toPact(V4Pact.class);
+            .usingLegacyDsl()
+            .given("A valid RedactionDTO with ID exists")
+            .uponReceiving("PUT request to update RedactionDTO")
+            .path(MARKUPS_API_PATH)
+            .method("PUT")
+            .headers(getHeaders())
+            .body(createRedactionDtoDsl())
+            .willRespondWith()
+            .status(HttpStatus.OK.value())
+            .headers(Map.of("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+            .body(createRedactionDtoDsl())
+            .toPact(V4Pact.class);
     }
 
     @Test
     @PactTestFor(pactMethod = "updateMarkUpPact")
     void testUpdateMarkUp(MockServer mockServer) {
-        String requestBody = String.format("""
-                {
-                  "redactionId": "%s",
-                  "documentId": "%s",
-                  "page": 1,
-                  "rectangles": [
-                    {
-                      "id": "%s",
-                      "x": 10.5,
-                      "y": 20.5,
-                      "width": 100.0,
-                      "height": 200.0
-                    }
-                  ]
-                }
-                """, REDACTION_ID, DOCUMENT_ID, RECTANGLE_ID);
-
         SerenityRest
-                .given()
-                .headers(getHeaders())
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(requestBody)
-                .put(mockServer.getUrl() + "/api/markups")
-                .then()
-                .statusCode(HttpStatus.OK.value())
-                .body("redactionId", equalTo(REDACTION_ID.toString()))
-                .body("documentId", equalTo(DOCUMENT_ID.toString()))
-                .body("rectangles[0].id", equalTo(RECTANGLE_ID.toString()))
-                .body("rectangles[0].x", equalTo(10.5f));
+            .given()
+            .headers(getHeaders())
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(createRedactionDtoDsl().getBody().toString())
+            .put(mockServer.getUrl() + MARKUPS_API_PATH)
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .body("redactionId", equalTo(REDACTION_ID.toString()))
+            .body("documentId", equalTo(DOCUMENT_ID.toString()))
+            .body("rectangles[0].id", equalTo(RECTANGLE_ID.toString()))
+            .body("rectangles[0].x", equalTo(10.5f));
     }
 
-
-    @Pact(consumer = "em_npa_api", provider = "native_pdf_annotator_api_provider")
+    @Pact(consumer = "em_npa_api", provider = NPA_PROVIDER)
     public V4Pact deleteMarkupsPact(PactBuilder builder) {
         return builder
-                .usingLegacyDsl()
-                .given("Markups exist for document ID and can be deleted")
-                .uponReceiving("DELETE request to remove all markups by document ID")
-                .path("/api/markups/" + DOCUMENT_ID)
-                .method("DELETE")
-                .headers(getHeaders())
-                .willRespondWith()
-                .status(200)
-                .matchHeader(
-                        "X-npaApp-alert",
-                        "A redaction is deleted with identifier " + DOCUMENT_ID)
-                .matchHeader(
-                        "X-npaApp-params",
-                        DOCUMENT_ID.toString())
-                .toPact(V4Pact.class);
+            .usingLegacyDsl()
+            .given("Markups exist for document ID and can be deleted")
+            .uponReceiving("DELETE request to remove all markups by document ID")
+            .path(MARKUPS_API_PATH + "/" + DOCUMENT_ID)
+            .method("DELETE")
+            .headers(getHeaders())
+            .willRespondWith()
+            .status(HttpStatus.OK.value())
+            .matchHeader(
+                "X-npaApp-alert",
+                "A redaction is deleted with identifier " + DOCUMENT_ID)
+            .matchHeader(
+                "X-npaApp-params",
+                DOCUMENT_ID.toString())
+            .toPact(V4Pact.class);
     }
 
     @Test
     @PactTestFor(pactMethod = "deleteMarkupsPact")
     void testDeleteMarkupsByDocumentId(MockServer mockServer) {
         SerenityRest
-                .given()
-                .headers(getHeaders())
-                .delete(mockServer.getUrl() + "/api/markups/" + DOCUMENT_ID)
-                .then()
-                .statusCode(HttpStatus.OK.value());
+            .given()
+            .headers(getHeaders())
+            .delete(mockServer.getUrl() + MARKUPS_API_PATH + "/" + DOCUMENT_ID)
+            .then()
+            .statusCode(HttpStatus.OK.value());
     }
 
-    @Pact(consumer = "em_npa_api", provider = "native_pdf_annotator_api_provider")
+    @Pact(consumer = "em_npa_api", provider = NPA_PROVIDER)
     public V4Pact deleteSingleMarkupPact(PactBuilder builder) {
+        String path = String.format("%s/%s/%s", MARKUPS_API_PATH, DOCUMENT_ID, REDACTION_ID);
         return builder
-                .usingLegacyDsl()
-                .given("A valid Redaction with document ID and redaction ID exists and can be deleted")
-                .uponReceiving("DELETE request to remove specific markup by document and redaction ID")
-                .path(String.format("/api/markups/%s/%s", DOCUMENT_ID, REDACTION_ID))
-                .method("DELETE")
-                .headers(getHeaders())
-                .willRespondWith()
-                .status(200)
-                .matchHeader(
-                        "X-npaApp-alert",
-                        "A redaction is deleted with identifier " + REDACTION_ID)
-                .matchHeader(
-                        "X-npaApp-params",
-                        REDACTION_ID.toString())
-                .toPact(V4Pact.class);
+            .usingLegacyDsl()
+            .given("A valid Redaction with document ID and redaction ID exists and can be deleted")
+            .uponReceiving("DELETE request to remove specific markup by document and redaction ID")
+            .path(path)
+            .method("DELETE")
+            .headers(getHeaders())
+            .willRespondWith()
+            .status(HttpStatus.OK.value())
+            .matchHeader(
+                "X-npaApp-alert",
+                "A redaction is deleted with identifier " + REDACTION_ID)
+            .matchHeader(
+                "X-npaApp-params",
+                REDACTION_ID.toString())
+            .toPact(V4Pact.class);
     }
 
     @Test
     @PactTestFor(pactMethod = "deleteSingleMarkupPact")
     void testDeleteSingleMarkup(MockServer mockServer) {
+        String path = String.format("%s/%s/%s", MARKUPS_API_PATH, DOCUMENT_ID, REDACTION_ID);
         SerenityRest
-                .given()
-                .headers(getHeaders())
-                .delete(mockServer.getUrl()
-                        + "/api/markups/f2cc4d79-d0f3-4b43-affe-535516370cdd/4c34ba4a-585a-407d-aa78-3f86f3171cdd")
-                .then()
-                .statusCode(200);
+            .given()
+            .headers(getHeaders())
+            .delete(mockServer.getUrl() + path)
+            .then()
+            .statusCode(HttpStatus.OK.value());
+    }
+
+    private void buildRedactionDto(LambdaDslObject root) {
+        root.uuid("redactionId", REDACTION_ID);
+        root.uuid("documentId", DOCUMENT_ID);
+        root.integerType("page", 1);
+        root.minArrayLike("rectangles", 1, rect -> {
+            rect.uuid("id", RECTANGLE_ID);
+            rect.numberType("x", 10.5);
+            rect.numberType("y", 20.5);
+            rect.numberType("width", 100.0);
+            rect.numberType("height", 200.0);
+        });
+    }
+
+    private DslPart createRedactionDtoDsl() {
+        return LambdaDsl.newJsonBody(this::buildRedactionDto).build();
+    }
+
+    private DslPart createRedactionDtoArrayDsl() {
+        return newJsonArray(array -> array.object(this::buildRedactionDto)).build();
     }
 
 }
